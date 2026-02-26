2ba27a8fed7efe928c81288de4d75a037d035e3b
sammysaqfelhait04 <ssaqfelhait@student.cscc.edu>
2026-02-25 21:35:27 -0500

    Delete unit_3 directory

--- unit_3/participation.py
+++
@@ -1,9 +0,0 @@
-import random
-
-names = ["dristhuth","gurtheos","kriurphax","rikrun","koskrain","tushkoc","raskejux", "tesgiujeks", "kabotre", "muduktith"]
-
-index = random.randint(0, len(names) - 1)
-greeting = f"Hello {names[index]}, it is good to see you again!"
-
-print(greeting)
-
